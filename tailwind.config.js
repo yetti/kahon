948793491a585fe3462eac5f08971f19d2c132e3
Yetrina Battad <hello@yetti.io>
2022-10-02 11:16:08 +1100

    style: implement custom typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        ...defaultTheme.fontFamily,
+        heading: ['Lora', ...defaultTheme.fontFamily.serif],
+        code: ['Roboto Mono', ...defaultTheme.fontFamily.mono],
+        normal: ['Open Sans', ...defaultTheme.fontFamily.sans],
+        sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+        serif: ['Lora', ...defaultTheme.fontFamily.serif],
+        mono: ['Roboto Mono', ...defaultTheme.fontFamily.mono],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
   ]
 }
