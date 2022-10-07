08acd4263a079d9cdebeaddf52b09a9b78837257
Yetrina Battad <hello@yetti.io>
2022-10-08 00:11:40 +1100

    feat: adds view components and lookbook for previews

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "pages/home"
 
+  if Rails.env.development?
+    mount Lookbook::Engine, at: "/lookbook"
+  end
+
   # Defines the root path route ("/")
   root "pages#home"
 end
