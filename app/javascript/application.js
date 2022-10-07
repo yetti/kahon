d4344f8a3c4d949180d6982a9053379b4042c793
Yetrina Battad <hello@yetti.io>
2022-10-08 00:23:42 +1100

    feat: adds unpoly

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+import "unpoly/unpoly.js"
+import "unpoly/unpoly.css"
+
+import "./pages"
