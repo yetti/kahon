f9004953a3f3efcb7862acf82ccda74de4cceeac
Yetrina Battad <hello@yetti.io>
2022-10-15 16:38:58 +1100

    feat: adds Vite alias for assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import FullReload from 'vite-plugin-full-reload'
 import StimulusHMR from 'vite-plugin-stimulus-hmr'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@assets': resolve(__dirname, 'app/assets'),
+    },
+  },
   plugins: [
     RubyPlugin(),
     StimulusHMR(),
